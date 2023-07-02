d730c9757b9ebd9c8e4e06c2f07878a8397f7b0d
yokochoyokocho <gotcha.pipi.new@gmail.com>
2023-07-02 15:30:17 +0900

    self_introduction_method_add

--- user_name.py
+++ user_name.py
@@ -1,4 +1,4 @@
-class UserName:
+class User:
     def __init__(self, name):
         if not (4 <= len(name) <= 20):
             raise ValueError(f"{name}はルール違反です")
@@ -7,11 +7,13 @@ class UserName:
     def screen_name(self):
         return self.name.upper()
 
+    def self_introduction(self):
+        return f"私の名前は{self.name.upper()}です"
+
+
 # UserNameクラスのインスタンス化
-tanaka = UserName(name="tanaka")
+tanaka = User(name="tanaka")
 
 # クラスの値の出力
 print(tanaka.screen_name())
-
-
-
+print(tanaka.self_introduction())
